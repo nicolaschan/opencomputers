c1e5816ffdc458aacc4b9f11dd783c2ffed6631f
Nicolas Chan <nicolas@nicolaschan.com>
2020-12-20 22:55:48 -0800

    Detect up

--- mine/mine.lua
+++ mine/mine.lua
@@ -22,32 +22,32 @@ print("Current energy: " .. computer.energy())
 local d = 0
 
 
-local function clearSideRaw(side, f)
-  local impass, desc = robot.detect(side)
+local function clearSideRaw(side, f, d)
+  local impass, desc = d()
   while impass do
     f()
-    impass, desc = robot.detect(side)
+    impass, desc = d()
   end
 end
 
 local function clearSide(side)
   if side == sides.front then
-    clearSideRaw(side, robot.swing)
+    clearSideRaw(side, robot.swing, robot.detect)
   elseif side == sides.top then
-    clearSideRaw(side, robot.swingUp)
+    clearSideRaw(side, robot.swingUp, robot.detectUp)
   elseif side == sides.bottom then
-    clearSideRaw(side, robot.swingDown)
+    clearSideRaw(side, robot.swingDown, robot.detectDown)
   elseif side == sides.left then
     robot.turnLeft()
-    clearSideRaw(side, robot.swing)
+    clearSideRaw(side, robot.swing, robot.detect)
     robot.turnRight()
   elseif side == sides.right then
     robot.turnRight()
-    clearSideRaw(side, robot.swing)
+    clearSideRaw(side, robot.swing, robot.detect)
     robot.turnLeft()
   elseif side == sides.back then
     robot.turnAround()
-    clearSideRaw(side, robot.swing)
+    clearSideRaw(side, robot.swing, robot.detect)
     robot.turnAround()
   end
 end

25ae692570908c6707af2a671d3208628e1eda20
Nicolas Chan <nicolas@nicolaschan.com>
2020-12-20 22:02:18 -0800

    Tunnel

--- mine/mine.lua
+++ mine/mine.lua
@@ -1,2 +1,20 @@
-print("hello i'm mining2")
-print(computer.energy())
+local component = require("component")
+local computer = require("computer")
+local robot = require("robot")
+local shell = require("shell")
+local sides = require("sides")
+
+print("Mine!")
+
+local args, options = shell.parse(...)
+print(args)
+print(options)
+
+local distance = tonumber(args[1])
+if not distance then
+  io.stdeer:write("invalid distance")
+  return
+end
+
+print("tunneling distance")
+print("Current energy: " .. computer.energy())
